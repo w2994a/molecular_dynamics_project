c201f361c0698dc213992f692200a8297fa1beb0
William AMORY <w_amory@orange.fr>
2022-03-05 15:25:42 +0100

    Add binder

--- canevas_test.py
+++
@@ -1,9 +0,0 @@
-import argparse, pathlib
-
-parser = argparse.ArgumentParser()
-parser.add_argument('file', type=pathlib.Path, help="mettre un file")
-parser.add_argument("-l", "--launch", help="Launch MD", action="store_true")
-args = parser.parse_args()
-
-with args.file.open('w') as file:
-    file.write("coucou")
